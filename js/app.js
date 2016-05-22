4d00c17c4c2e4110449d6fd9f9c03b7f51d53891
ayushgosi <anime.ayush@gmail.com>
2016-05-23 02:46:52 +0530

    Initial CricBase Commit

---
+++ js/app.js
@@ -0,0 +1,61 @@
+//Modules
+var cricBase = angular.module('cricBase', ['ngRoute', 'ngResource', 'ngAnimate', 'firebase']);
+
+//Routes
+cricBase.config(function($routeProvider){
+  $routeProvider
+
+  .when('/', {
+    templateUrl: 'pages/main.htm',
+    controller: 'mainController',
+    controllerAs: 'main'
+  })
+
+    .when('/form', {
+      templateUrl: 'pages/form.htm',
+      controller: 'formController',
+      controllerAs: 'form'
+    })
+});
+
+//Services
+
+
+//Controllers
+cricBase.controller('mainController',['$scope', '$firebaseArray', '$resource', function($scope, $firebaseArray, $resource){
+  var vm = this;
+
+  var ref = new Firebase('https://cricbase626262.firebaseio.com/Matches');
+  vm.matches = $firebaseArray(ref);
+
+
+}]);
+
+cricBase.controller('formController', ['$scope', '$firebaseArray', '$resource', '$timeout', function($scope, $firebaseArray, $resource, $timeout){
+  var vm = this;
+
+  var ref = new Firebase('https://cricbase626262.firebaseio.com/Matches');
+  vm.matchData = $firebaseArray(ref);
+
+  function match(){
+    this.firstTeam = '';
+    this.secondTeam = '';
+    this.city = '';
+    this.stadium = '';
+    this.date = '';
+    this.format = '';
+    this.league = '';
+  }
+  vm.newMatch = new match();
+  vm.bool = false;
+
+  vm.addMatch = function(){
+    vm.matchData.$add(vm.newMatch);
+    vm.newMatch = new match();
+    // vm.bool = true;
+    // $timeout(function(){
+    //   vm.bool = false;
+    // },2000);
+  }
+
+}]);
